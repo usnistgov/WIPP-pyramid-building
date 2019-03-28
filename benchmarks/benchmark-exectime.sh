ae22fda0d52019b527086f722bd3bc07888f89aa
gerardin <antoine.gerardin@nist.gov>
2019-03-28 13:09:23 -0400

    graph behavior has changes because Read is longer a bottleneck.

--- benchmarks/benchmark-exectime.sh
+++ benchmarks/benchmark-exectime.sh
@@ -13,11 +13,11 @@
 -o /home/gerardin/Documents/pyramidBuilding/outputs \
 -t 1024 -d 8U
 
-../tools/run_benchmarks.sh dataset7_1024 5 --benchmark exectime  \
--v /home/gerardin/Documents/images/dataset7/manualStitching/img-global-positions-0.txt \
--i /home/gerardin/Documents/images/dataset7/tiled-images \
--o /home/gerardin/Documents/pyramidBuilding/outputs \
--t 1024 -d 8U
+#../tools/run_benchmarks.sh dataset7_1024 5 --benchmark exectime  \
+#-v /home/gerardin/Documents/images/dataset7/manualStitching/img-global-positions-0.txt \
+#-i /home/gerardin/Documents/images/dataset7/tiled-images \
+#-o /home/gerardin/Documents/pyramidBuilding/outputs \
+#-t 1024 -d 8U
 
 #../tools/run_benchmarks.sh dataset1_256 1 \
 #-k exectime  \
