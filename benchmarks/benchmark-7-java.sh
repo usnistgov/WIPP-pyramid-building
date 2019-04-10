3ad58e4bef5c9e9d1181a4a77ee10d3742cf3868
gerardin <antoine.gerardin@nist.gov>
2019-04-10 17:15:39 -0400

    improved benchmarks

--- benchmarks/benchmark-7-java.sh
+++ benchmarks/benchmark-7-java.sh
@@ -4,20 +4,44 @@ ROOT_DIR=/home/gerardin/Documents/pyramidBuilding
 EXEC_PATH=/home/gerardin/Documents/pyramidio-java/pyramidio/target
 OUTPUT_DIR=$ROOT_DIR/benchmarks/results
 
+#
+#
+#../tools/run_benchmarks_java.sh dataset5_java_1024 1 $EXEC_PATH $OUTPUT_DIR \
+#-v /home/gerardin/Documents/images/dataset5/img-global-positions-1.txt \
+#-i /home/gerardin/Documents/images/dataset5/images \
+#-o /home/gerardin/Documents/pyramidio-java/outputs \
+#-t 1024 -d 8U
 
-
-../tools/run_benchmarks_java.sh dataset5_java_1024 5 $EXEC_PATH $OUTPUT_DIR \
--v /home/gerardin/Documents/images/dataset5/img-global-positions-1.txt \
--i /home/gerardin/Documents/images/dataset5/images \
--o /home/gerardin/Documents/pyramidio-java/outputs \
--t 1024 -d 8U
-
-
-../tools/run_benchmarks_java.sh dataset7_java_1024 5 $EXEC_PATH $OUTPUT_DIR \
--v /home/gerardin/Documents/images/dataset7/img-global-positions-0.txt \
+../tools/run_benchmarks_java.sh java_dataset7_1024 1 $EXEC_PATH $OUTPUT_DIR \
+-v /home/gerardin/Documents/images/dataset7/manualStitching/img-global-positions-0.txt \
 -i /home/gerardin/Documents/images/dataset7/tiled-images \
 -o /home/gerardin/Documents/pyramidio-java/outputs \
--t 1024 -d 8U
+-t 1024 -d 8U --cache 0 --threads 5
+
+../tools/run_benchmarks_java.sh java_dataset7_1024 1 $EXEC_PATH $OUTPUT_DIR \
+-v /home/gerardin/Documents/images/dataset7/manualStitching/img-global-positions-0.txt \
+-i /home/gerardin/Documents/images/dataset7/tiled-images \
+-o /home/gerardin/Documents/pyramidio-java/outputs \
+-t 1024 -d 8U --cache 0 --threads 10
+
+../tools/run_benchmarks_java.sh java_dataset7_1024 1 $EXEC_PATH $OUTPUT_DIR \
+-v /home/gerardin/Documents/images/dataset7/manualStitching/img-global-positions-0.txt \
+-i /home/gerardin/Documents/images/dataset7/tiled-images \
+-o /home/gerardin/Documents/pyramidio-java/outputs \
+-t 1024 -d 8U --cache 0 --threads 20
+
+../tools/run_benchmarks_java.sh java_dataset7_1024 1 $EXEC_PATH $OUTPUT_DIR \
+-v /home/gerardin/Documents/images/dataset7/manualStitching/img-global-positions-0.txt \
+-i /home/gerardin/Documents/images/dataset7/tiled-images \
+-o /home/gerardin/Documents/pyramidio-java/outputs \
+-t 1024 -d 8U --cache 0 --threads 30
+
+../tools/run_benchmarks_java.sh java_dataset7_1024 1 $EXEC_PATH $OUTPUT_DIR \
+-v /home/gerardin/Documents/images/dataset7/manualStitching/img-global-positions-0.txt \
+-i /home/gerardin/Documents/images/dataset7/tiled-images \
+-o /home/gerardin/Documents/pyramidio-java/outputs \
+-t 1024 -d 8U --cache 0 --threads 50
+
 
 #ROOT_DIR=/Users/gerardin/Documents/projects/wipp++/pyramid-building/
 #EXEC_PATH=/Users/gerardin/Documents/projects/pyramidio/pyramidio/target/
